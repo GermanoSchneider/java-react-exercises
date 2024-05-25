16579b3fcd08693d0bcf666b51dfa48851962aa5
Germano Schneider <germanoschneider87@gmail.com>
2024-05-25 15:46:02 -0300

    maven project creation

---
+++ exercise_1/normalized-job-title/src/main/java/com/feefo/app/App.java
@@ -0,0 +1,13 @@
+package com.feefo.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
