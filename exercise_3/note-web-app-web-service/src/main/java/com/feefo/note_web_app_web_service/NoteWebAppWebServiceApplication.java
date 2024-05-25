35bfc6ba6af43443ab4153705175d82b306f08ca
Germano Schneider <germanoschneider87@gmail.com>
2024-05-25 16:52:03 -0300

    creation of the note-web-app web service

---
+++ exercise_3/note-web-app-web-service/src/main/java/com/feefo/note_web_app_web_service/NoteWebAppWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.feefo.note_web_app_web_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteWebAppWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteWebAppWebServiceApplication.class, args);
+	}
+
+}
---
+++ exercise_3/note-web-app-web-service/src/test/java/com/feefo/note_web_app_web_service/NoteWebAppWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feefo.note_web_app_web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteWebAppWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
